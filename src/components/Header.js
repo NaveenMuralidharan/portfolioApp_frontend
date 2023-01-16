ee4b96b8ba02b4a71ad9b7b1a442e5dd287f1d44
Naveen Muralidharan <naveenspec@gmail.com>
2023-01-15 19:53:16 -0500

    corrected build errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { findByLabelText } from "@testing-library/react"
+
 import { Link } from "react-router-dom"
 
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,16 +6,16 @@ function Projects (props) {
     const projects = useLoaderData()
 
     return projects.map((project)=>{
-        <div>
+        return (<div key={project._id}>
             <h1> { project.name } </h1>
-                <img src={project.image} />
+                <img src={project.image} alt={project.name} />
             <a href={project.git}>
                 <button>Github</button>
             </a>
             <a href={project.live}>
                 <button>live site</button>
             </a>
-        </div>
+        </div>)
     })
 }
 
